6a3d0f32d9e8d81074c32cd6a2ffc65b08ad1b7e
cloudBruhh <ashwinthapa34009@gmail.com>
2026-02-02 18:52:44 +0545

    matrix sum

--- matsum-func.c
+++ matsum-func.c
@@ -18,9 +18,9 @@ void input(int a[N][N])
     printf("Enter the number");
     scanf("%d", &n);
     printf("Enter elements of the matrix:\n");
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < 3; j++)
+        for (j = 0; j < n; j++)
         {
             printf("Enter element a%d%d: ", i + 1, j + 1);
             scanf("%d", &a[i][j]);
@@ -29,10 +29,10 @@ void input(int a[N][N])
 }
 void process(int a[N][N], int b[N][N], int c[N][N])
 {
-    int i, j;
-    for (i = 0; i < 3; i++)
+    int i, j, n;
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < 3; j++)
+        for (j = 0; j < n; j++)
         {
             c[i][j] = a[i][j] + b[i][j];
         }
@@ -40,11 +40,11 @@ void process(int a[N][N], int b[N][N], int c[N][N])
 }
 void output(int c[N][N])
 {
-    int i, j;
+    int i, j, n;
     printf("Resultant Matrix after addition:\n");
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < 3; j++)
+        for (j = 0; j < n; j++)
         {
             printf("%d\t", c[i][j]);
         }
