761defe01283fb7c0e6f7e96bf089120252523e3
cloudBruhh <ashwinthapa34009@gmail.com>
2026-02-16 15:11:22 +0545

    armstrong

--- armstrong.c
+++ armstrong.c
@@ -1,32 +1,30 @@
-#include <stdio.h>
+// This is a comment
 #include <math.h>
-int main()
-{
-    int num, temp, rem, digits = 0, res = 0;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-    temp = num;
+#include <stdio.h>
+int main() {
+  int num, temp, rem, digits = 0, res = 0;
+  printf("Enter an integer: ");
+  scanf("%d", &num);
+  temp = num;
 
-    // Count number of digits
-    while (temp != 0)
-    {
-        temp /= 10;
-        digits++;
-    }
+  // Count number of digits
+  while (temp != 0) {
+    temp /= 10;
+    digits++;
+  }
 
-    temp = num;
-    // Calculate Armstrong number
-    while (temp != 0)
-    {
-        rem = temp % 10;
-        res += pow(rem, digits);
-        temp /= 10;
-    }
+  temp = num;
+  // Calculate Armstrong number
+  while (temp != 0) {
+    rem = temp % 10;
+    res += pow(rem, digits);
+    temp /= 10;
+  }
 
-    if (res == num)
-        printf("%d is an Armstrong number.\n", num);
-    else
-        printf("%d is not an Armstrong number.\n", num);
+  if (res == num)
+    printf("%d is an Armstrong number.\n", num);
+  else
+    printf("%d is not an Armstrong number.\n", num);
 
-    return 0;
+  return 0;
 }
