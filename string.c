4e7d018dcaebde8c34c045f03008e8f9f96a6b60
cloudBruhh <ashwinthapa34009@gmail.com>
2026-02-27 19:21:23 +0545

    Reversed string

--- string.c
+++ string.c
@@ -2,25 +2,25 @@
 #include <string.h>
 
 int main() {
-    char str[1000];
+  char str[1000];
 
-    printf("Enter text: ");
-    fgets(str, sizeof(str), stdin);
+  printf("Enter text: ");
+  fgets(str, sizeof(str), stdin);
 
-    /* Strip trailing newline from fgets */
-    int len = strlen(str);
-    if (len > 0 && str[len - 1] == '\n') {
-        str[--len] = '\0';
-    }
+  /* Strip trailing newline from fgets */
+  int len = strlen(str);
+  if (len > 0 && str[len - 1] == '\n') {
+    str[--len] = '\0';
+  }
 
-    /* Reverse in-place by swapping from both ends */
-    for (int i = 0; i < len / 2; i++) {
-        char temp       = str[i];
-        str[i]          = str[len - 1 - i];
-        str[len - 1 - i] = temp;
-    }
+  /* Reverse in-place by swapping from both ends */
+  for (int i = 0; i < len / 2; i++) {
+    char temp = str[i];
+    str[i] = str[len - 1 - i];
+    str[len - 1 - i] = temp;
+  }
 
-    printf("Reversed: %s\n", str);
+  printf("Reversed string is: %s\n", str);
 
-    return 0;
+  return 0;
 }
